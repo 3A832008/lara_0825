b7b899d931b254d87d148587ba04d006c4704cd0
{hui lin chu} <3a832008@gm.student.ncut.edu.tw>
2021-11-10 11:34:32 +0800

    使用all方法

--- routes/web.php
+++ routes/web.php
@@ -20,14 +20,13 @@ Route::get('/', function () {
     //$post->content = 'test content';
     //$post->save();
 });
+
+//Post::create([
+//        'title'=>'test title2',
+//'content'=>'test content2'
+//]);
 $post=\App\Models\Post::all();
 dd($post);
-
-Post::create([
-        'title'=>'test title2',
-        'content'=>'test content2'
-]);
-
 Route::get('posts', [\App\Http\Controllers\PostsController::class, 'index'])->name('posts.index');
 Route::get('post', [\App\Http\Controllers\PostsController::class, 'show'])->name('posts.show');
 Route::get('contact', [\App\Http\Controllers\PostsController::class, 'contact'])->name('posts.contact');
