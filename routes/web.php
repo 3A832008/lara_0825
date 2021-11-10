0f467b218b9ede9b6ba3b1ca12ae044ae31d5cdc
{hui lin chu} <3a832008@gm.student.ncut.edu.tw>
2021-11-10 11:53:21 +0800

    取得Collection

--- routes/web.php
+++ routes/web.php
@@ -43,7 +43,11 @@ Route::get('/', function () {
 //$post=Post::find(1);
 //$post->delete();
 //Post::destroy(2);
-Post::destroy(3, 5, 7);
+//Post::destroy(3, 5, 7);
+$allPosts=Post::all();
+dd($allPosts);
+
+
 
 Route::get('posts', [\App\Http\Controllers\PostsController::class, 'index'])->name('posts.index');
 Route::get('post', [\App\Http\Controllers\PostsController::class, 'show'])->name('posts.show');
