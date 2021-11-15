42957485cb08f1aeb86a9d56aad7071dedb30e1c
{hui lin chu} <3a832008@gm.student.ncut.edu.tw>
2021-11-15 21:46:21 +0800

    練習7-3 增加與Post的關係

--- app/Models/Comment.php
+++ app/Models/Comment.php
@@ -2,18 +2,19 @@
 
 namespace App\Models;
 
+use App\Models\Post;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 
-class Post extends Model
+class Comment extends Model
 {
     use HasFactory;
-    protected $table = 'posts';
-    protected $fillable = [
-        'title',
-        'content',
-        'is_feature'
-    ];
+
+    public function post()
+    {
+        return $this->belongsTo(Post::class);
+    }
 
 }
+
 
